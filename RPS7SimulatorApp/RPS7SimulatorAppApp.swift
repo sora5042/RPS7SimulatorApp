44a38ce19d2b854c86e5e604ed899f306888364c
sora5042 <esuora5042@gmail.com>
2025-03-03 18:31:17 +0900

    Initial Commit

---
+++ RPS7SimulatorApp/RPS7SimulatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS7SimulatorAppApp.swift
+//  RPS7SimulatorApp
+//
+//  Created by Sora Oya on 2025/03/03.
+//
+
+import SwiftUI
+
+@main
+struct RPS7SimulatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPS7SimulatorAppTests/RPS7SimulatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPS7SimulatorAppTests.swift
+//  RPS7SimulatorAppTests
+//
+//  Created by Sora Oya on 2025/03/03.
+//
+
+import Testing
+@testable import RPS7SimulatorApp
+
+struct RPS7SimulatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
